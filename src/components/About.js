0cc13994b4d27493d64813ee8f4704713d9cfef9
Juniper James <vvlpespro@gmail.com>
2023-02-16 21:05:22 -0500

    prepare for deployment

--- src/components/About.js
+++ src/components/About.js
@@ -3,17 +3,18 @@ import React from 'react'
 function About() {
   return (
       <div className='content'>
-            <h1>Hello!</h1>
-        <div className='first'>
-        <p> my Name is June :] I am a full-stack web developer, game designer,and graphic artist. I love making interactive and stylish applications. 
-            </p></div>
-            <div className='second'>
+            <h1 className='first'>Hello!</h1>
+        <div className='content second'>
+        <p className='first'> my Name is June :] I am a full-stack web developer, game designer,and graphic artist. I love making interactive and stylish applications. 
+            </p>
+            <p className='second'>
                 -javascript -css -html -node.js -express -mongoose/mongodb -ejs -react -aseprite -django -python -excel -gimp
-            </div>
+            </p>
             <div className='third'>
                 
-            <p > -find me on github </p>
-            <p > -connect on linkedin</p>
+            <p> -find me on github </p>
+            <p> -connect on linkedin</p>
+            </div>
             </div>
         </div>
   )
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,13 @@ import React from 'react'
 function Contact() {
   return (
     <div className='content'>
-        <h1>Contact</h1>
-        <p className='first'>-email </p><p className='second'>-linkedin</p><p className='third'>-discord</p>
+
+    <h1 className='first'>Contact</h1>
+    <div className='content'>
+        <p className='first'>-email </p>
+        <p className='second'>-linkedin</p>
+        <p className='third'><img src='/discord.png'className='icon' />discord</p>
+    </div>
     </div>
   )
 }
